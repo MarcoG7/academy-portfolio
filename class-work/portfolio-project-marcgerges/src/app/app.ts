430f899c2972f644a6f281817b287ea54e2ded91
Marc <63096791+MarcoG7@users.noreply.github.com>
2026-02-06 15:30:58 +0200

    Add about section to angular page

--- class-work/portfolio-project-marcgerges/src/app/app.ts
+++ class-work/portfolio-project-marcgerges/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import { Profile } from './section/profile/profile';
+import { About } from './section/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Profile],
+  imports: [RouterOutlet, Navbar, Profile, About],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
