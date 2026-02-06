16b6fae37478c72b44cc911e02fc3fd8ab82bae7
Marc <63096791+MarcoG7@users.noreply.github.com>
2026-02-06 15:23:08 +0200

    Add profile section to page

--- class-work/portfolio-project-marcgerges/src/app/app.ts
+++ class-work/portfolio-project-marcgerges/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
+import { Profile } from './section/profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Profile],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
